34c8474d950113bb3e027f8c52a7b2c835dc623b
wajahat <wajahatlisyed@gmail.com>
2025-07-11 16:26:15 +0500

    Updated main for array

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Car my_Car= new Car("Kia Sportage","Kia Corporation","White","SUV");
         Car mini_Car= new Car("Civic","honda","Black","sedan");
-        System.out.println("For "+my_Car.name);
-        my_Car.display_Car();
-        System.out.println("For "+ mini_Car.name);
-        mini_Car.display_Car();
+
+       Car[] array= {my_Car,mini_Car};
+        for (Car car : array) {
+            car.display_Car();
+        }
+
     }
 }
